a6579b9235a2825a34bc8c7f2916cdbaeddc48c9
yangjing <yangjing@huohua.cn>
2020-07-15 21:00:23 +0800

    Updates

---
+++ precache-manifest.dc70c2c7ef85124a7804053e3db68ece.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "316bc4fad7579569ed3e23dc09dd3cd8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "22b045817bdeef915bae",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "90e52c57d37746d906fc",
+    "url": "./static/js/2.763b34e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.763b34e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22b045817bdeef915bae",
+    "url": "./static/js/main.4a5c5b72.chunk.js"
+  },
+  {
+    "revision": "ca8051aa2fe5b3ef84c3",
+    "url": "./static/js/runtime-main.673723db.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
