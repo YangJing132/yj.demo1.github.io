4c9680cf298a3887a66fc65070f56d5d5dc43926
yangjing <yangjing@huohua.cn>
2020-07-15 21:42:21 +0800

    修改文字

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          编辑 <code>src/App.js</code> and save to reload.
+          编辑 <code>src/App.js</code> and save to 重新加载.
         </p>
         <a
           className="App-link"
